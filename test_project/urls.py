1fdb0c7b3d7cc1c37b29d82263372e21dec0eb83
Jacek <jacek@ivolution.pl>
2014-02-10 12:22:35 +0100

    urls

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf import settings
 try:
     from django.conf.urls import patterns, url, include
 except ImportError:
@@ -9,7 +10,13 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r"^admin/", include(admin.site.urls)),)
+    url(r"^admin/", include(admin.site.urls)),
+    (r'^%s(?P<path>.*)$' % settings.STATIC_URL[1:],  # cut away leading slash
+         'django.views.static.serve', {'document_root': settings.STATIC_ROOT,
+                                       'show_indexes': True}),
+    (r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],  # cut away leading slash
+         'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,
+                                       'show_indexes': False}),)
 #    url(r"^js-tests/(?P<path>.*)",
 #        'contactBox.views.qunit_view',
 #       name='qunit'),)
