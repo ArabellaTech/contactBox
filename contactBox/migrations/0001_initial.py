5dbe684c8338571702b33b7b17a86462eab48a05
Jacek <jacek@ivolution.pl>
2015-12-17 16:57:28 +0100

    django 1.8 support

--- contactBox/migrations/0001_initial.py
+++ contactBox/migrations/0001_initial.py
@@ -1,65 +1,39 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Message'
-        db.create_table(u'contactBox_message', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('organization', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('phone', self.gf('django.db.models.fields.CharField')(max_length=20, blank=True)),
-            ('message', self.gf('django.db.models.fields.TextField')()),
-            ('notification_date', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
-            ('unread', self.gf('django.db.models.fields.BooleanField')(default=True)),
-        ))
-        db.send_create_signal(u'contactBox', ['Message'])
+    dependencies = [
+    ]
 
-        # Adding model 'Receiver'
-        db.create_table(u'contactBox_receiver', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
-        ))
-        db.send_create_signal(u'contactBox', ['Receiver'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Message'
-        db.delete_table(u'contactBox_message')
-
-        # Deleting model 'Receiver'
-        db.delete_table(u'contactBox_receiver')
-
-
-    models = {
-        u'contactBox.message': {
-            'Meta': {'ordering': "['-id']", 'object_name': 'Message'},
-            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'message': ('django.db.models.fields.TextField', [], {}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'notification_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'organization': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'phone': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
-            'unread': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
-        },
-        u'contactBox.receiver': {
-            'Meta': {'object_name': 'Receiver'},
-            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['contactBox']
+    operations = [
+        migrations.CreateModel(
+            name='Message',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date', models.DateTimeField(help_text=b'Creation date', auto_now_add=True)),
+                ('name', models.CharField(max_length=255, verbose_name=b'Name')),
+                ('email', models.EmailField(max_length=254, verbose_name=b'Email address')),
+                ('organization', models.CharField(max_length=255, verbose_name=b'Organization', blank=True)),
+                ('phone', models.CharField(max_length=20, verbose_name=b'Phone', blank=True)),
+                ('message', models.TextField(verbose_name=b'Comments')),
+                ('notification_date', models.DateTimeField(help_text=b'When notification was send', null=True, editable=False, blank=True)),
+                ('unread', models.BooleanField(default=True)),
+            ],
+            options={
+                'ordering': ['-id'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Receiver',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('email', models.EmailField(max_length=254)),
+                ('active', models.BooleanField(default=True)),
+            ],
+        ),
+    ]
