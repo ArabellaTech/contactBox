d45d5ce046be82c335275271ffda617da2af8517
Jacek <jacek@ivolution.pl>
2016-01-04 12:56:13 +0100

    updated tests

--- contactBox/tests/main_test.py
+++ contactBox/tests/main_test.py
@@ -1,6 +1,32 @@
 from django.test import TestCase
+from django.core.management import call_command
+from django.core import mail
+
+from contactbox.models import Message, Receiver
 
 
 class MainTestCase(TestCase):
     def setUp(self):
-        
+        Receiver.objects.create(
+            name='test',
+            email='foo@bar.com',
+            active=True)
+        Receiver.objects.create(
+            name='test',
+            email='foo1@bar.com',
+            active=True)
+        Receiver.objects.create(
+            name='test',
+            email='foo2@bar.com',
+            active=False)
+
+    def test_sending(self):
+        message = Message.objects.create(
+            email='a@a.com',
+            message='message',
+        )
+        self.assertEqual(mail.outbox, [])
+        self.assertIsNone(message.notification_date)
+        call_command('remind_contact')
+        self.assertIsNorNone(message.notification_date)
+        self.assertEqual(len(mail.outbox), 1)
